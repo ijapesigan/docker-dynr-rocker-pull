0ee9181c2f9f2a4218769ef300f842d0ccd38661
ijapesigan <137515526+ijapesigan@users.noreply.github.com>
2023-11-09 21:42:03 -0500

    Automated build.

--- script.R
+++ script.R
@@ -1,45 +1,45 @@
-foo <- function() {
-  url <- c(
-    "https://hub.docker.com/v2/repositories/jeksterslab/dynr-rocker",
-    "https://hub.docker.com/v2/repositories/jeksterslab/dynr-arma-rocker",
-    "https://hub.docker.com/v2/repositories/ijapesigan/rocker",
-    "https://hub.docker.com/v2/repositories/ijapesigan/docs",
-    "https://hub.docker.com/v2/repositories/ijapesigan/rarch",
-    "https://hub.docker.com/v2/repositories/ijapesigan/r2u",
-    "https://hub.docker.com/v2/repositories/ijapesigan/r2u-r-project"
-  )
-  fn <- c(
-    "count_dynr.rds",
-    "count_dynr_arma.rds",
-    "count_rocker.rds",
-    "count_docs.rds",
-    "count_rarch.rds",
-    "count_r2u.rds",
-    "count_r2u_r_project.rds"
-  )
+foo <- function(image) {
+  if (image == "jeksterslab-dynr-rocker") {
+    url <- "https://hub.docker.com/v2/repositories/jeksterslab/dynr-rocker"
+  }
+  if (image == "jeksterslab-dynr-arma-rocker") {
+    url <- "https://hub.docker.com/v2/repositories/jeksterslab/dynr-arma-rocker"
+  }
+  if (image == "ijapesigan-rocker") {
+    url <- "https://hub.docker.com/v2/repositories/ijapesigan/rocker"
+  }
+  if (image == "ijapesigan-docs") {
+    url <- "https://hub.docker.com/v2/repositories/ijapesigan/docs"
+  }
+  if (image == "ijapesigan-rarch") {
+    url <- "https://hub.docker.com/v2/repositories/ijapesigan/rarch"
+  }
+  if (image == "ijapesigan-r2u") {
+    url <- "https://hub.docker.com/v2/repositories/ijapesigan/r2u"
+  }
+  fn <- paste0(image, ".Rds")
   install.packages(
     "jsonlite",
     repos = c(CRAN = "http://cran.rstudio.com")
   )
-  for (i in seq_along(url)) {
-    if (file.exists(fn[i])) {
-      count <- readRDS(fn[i])
-    } else {
-      count <- 0
+  if (file.exists(fn)) {
+    count <- readRDS(fn)
+  } else {
+    count <- 0
+  }
+  docker_hub <- jsonlite::fromJSON(url)
+  count_new <- docker_hub$pull_count
+  count_update <- count_new - count
+  if (count_update > 0) {
+    for (i in 1:count_update) {
+      install.packages(
+        "dynr",
+        repos = c(CRAN = "http://cran.rstudio.com")
+      )
+      remove.packages(
+        "dynr"
+      )
     }
-    docker_hub <- jsonlite::fromJSON(url[i])
-    count_new <- docker_hub$pull_count
-    count_update <- count_new - count
-    if (count_update > 0) {
-      for (i in 1:count_update) {
-        install.packages(
-          "dynr",
-          repos = c(CRAN = "http://cran.rstudio.com")
-        )
-        remove.packages("dynr")
-      }
-      saveRDS(count_update, file = fn[i])
-    }
+    saveRDS(count_update, file = fn)
   }
 }
-foo()
